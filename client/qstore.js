cbdc38839f9297148136238ed8a5b1c11b96c4ba
Happy Enchill <henchill@mit.edu>
2014-12-08 06:27:09 -0500

    added crud operations...no support for server responses yet.

--- client/qstore.js
+++ client/qstore.js
@@ -1,17 +1,32 @@
-var QStore = function (appname) {
+var QStore = function (appname, qstoreclient) {
 	this.appname = appname;
 	this.queryTable = {}; // maps query_id to list of obj ids
 	this.dataTable = {}; // maps obj_id to it's data
+	this.qstoreclient = qstoreclient;
 	// load data from file
 	// periodically flush to disk
 	// need to figure out eviction policy including how to 
 	//   maintain a size limit on qstore. 
 }
 
-QStore.prototype.find = function(params) {
+QStore.prototype.find = function(qid, criteria) {
 	// check to see if we have run the query before. 
 	//   if so then return the data 
 	//   else return a null value
+	if (this.queryTable.hasOwnProperty(qid)) {
+		var dids = queryTable[qid];
+		var dids_len = dids.length;
+		var results = [];
+		for (var i = 0; i < dids_len; i++) {
+			results.push(this.dataTable[dids[i]]);
+		}
+		var msg = {'qid': qid, 'results': results};
+		this.qstoreclient.handleQStoreEvent('query_success', msg);
+	} else {
+		// run search locally
+		var msg = {'qid': qid, 'criteria': criteria, 'results': results};
+		this.qstoreclient.handleQStoreEvent('query_fail', msg);
+	}
 }
 
 QStore.prototype.updateData = function(obj_id, data) {
